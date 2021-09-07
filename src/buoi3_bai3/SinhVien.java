59b743d66369ab98c9291187674e35e590252725
Tarykege <xoiiwkingg@gmail.com>
2021-09-07 18:16:28 +0700

    initialized_SinhVien_class

--- src/buoi3_bai3/SinhVien.java
+++ src/buoi3_bai3/SinhVien.java
@@ -1,5 +1,50 @@
 package buoi3_bai3;
 
-public class SinhVien {
+import buoi2_bai2.Date;
 
+public class SinhVien {
+	String mssv;
+	String name;
+	Date ngaySinh;
+	int soHocPhan;
+	String[] dsHocPhan;
+	String[] diemHocPhan;
+	
+	public SinhVien() {
+	}
+	public SinhVien(String mssv, String name, Date ngaySinh, int soHocPhan, String[] dsHocPhan, String[] diemHocPhan) {
+		this.mssv= mssv;
+		this.name=name;
+		this.ngaySinh= ngaySinh;
+		this.soHocPhan= soHocPhan;
+		this.dsHocPhan= dsHocPhan;
+		this.diemHocPhan= diemHocPhan;
+	}
+	
+	public void nhapSV() {
+
+	}
+	
+	public void nhapDiemHocPhan() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+	
+	float diemTB4() {
+		return 0;
+	}
+	
+	void dangKyHocPhan() {
+		
+	}
+	
+	void xoaHocPhan() {
+	
+	}
+	
+	
 }
