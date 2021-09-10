80111cad069f4970877ea32f9945b9d7e279823a
Tarykege <xoiiwkingg@gmail.com>
2021-09-10 12:08:37 +0700

    Complete_tinhGocTrucHoanh()

--- src/buoi3_bai2/DoanThang.java
+++ src/buoi3_bai2/DoanThang.java
@@ -20,7 +20,7 @@ public class DoanThang {
 		this.d1= new Diem(ax,ay);
 		this.d2= new Diem(bx,by);
 	}
-	
+//======================================================================//		
 	public void nhapToaDo() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Nhap diem A");
@@ -39,34 +39,95 @@ public class DoanThang {
 		int by= scanner.nextInt();
 		this.d2.setY(by);
 	}
-	
+//======================================================================//		
 	public void hienThi() {
 		this.d1.hienThi();
 		this.d2.hienThi();
 	}
-	
+//======================================================================//		
 	public void tinhTienDoanThang(int dx, int dy) {
 		this.d1.doiDiem(dx, dy);
 		this.d2.doiDiem(dx, dy);
 	}
-	
+//======================================================================//		
 	public float tinhDoDai() {
 		//sqrt((ax-bx)^2+(ay-by)^2)
 		return d1.khoangCach(d2);
 	}
-	
-//	public float tinhGocTrucHoanh() {
-//		Diem o = new Diem(0,0);
-//		DoanThang od1= new (o,)
-//		DoanThang od2= new (o);
-//		return 0;
-//	}
+//======================================================================//
+	public boolean checkDoiXung() {
+		Diem absA = new Diem(Math.abs(this.d1.giaTriX()),Math.abs(this.d1.giaTriY()));
+		Diem absB = new Diem(Math.abs(this.d2.giaTriX()),Math.abs(this.d2.giaTriY()));
+		if(absA.giaTriX()==absB.giaTriX() && absA.giaTriY()==absB.giaTriY()) {
+			return true;
+		} 
+		else return false;
+	}
+//======================================================================//
+	public boolean checkDoiXungTrucX() {
+		if(checkDoiXung()==true && this.d1.giaTriX()*this.d2.giaTriX()>0 ) {
+			return true;
+		}
+		else return false;
+	}
+//======================================================================//	
+	public boolean checkDoiXungTrucY() {
+		
+		if(checkDoiXung()==true && this.d1.giaTriX()*this.d2.giaTriX()<0 ) {
+			return true;
+		}
+		else return false;
+	}
+//======================================================================//	
+	public boolean checkNamTrenTrucX() {
+		if(this.d1.giaTriX()*this.d2.giaTriX()<0 && (d1.giaTriY()==0 || d2.giaTriY()==0) ) {
+			return true;
+		}
+		else return false;
+	}
+//======================================================================//
+	public boolean checkNamTrenTrucY() {
+		if(this.d1.giaTriX()*this.d2.giaTriX()<=0 && (d1.giaTriX()==0 || d2.giaTriX()==0) ) {
+			return true;
+		}
+		else return false;
+	}
+//======================================================================//
+	public boolean checkTrungDiem() {
+		if(this.d1.giaTriX()==this.d2.giaTriX() && this.d1.giaTriY()==this.d2.giaTriY() ) {
+			return true;
+		}
+		else return false;
+	}
+//======================================================================//		
+	public float tinhGocTrucHoanh() {
+		if(checkTrungDiem() || checkDoiXungTrucY() || checkNamTrenTrucY()) return 0;
+		if(checkDoiXungTrucX() || checkNamTrenTrucX()) return 90;
+		Diem o = new Diem(0,0);
+		Diem a = new Diem(this.d1.giaTriX(),0);
+		Diem b = new Diem(0,this.d2.giaTriY());
+		
+		if(d2.giaTriY()==0 || d1.giaTriX()==0){
+			a = new Diem(this.d2.giaTriX(),0);
+			b = new Diem(0,this.d1.giaTriY());
+		}
+		
+		float oa= o.khoangCach(a);
+		float ob= o.khoangCach(b);
+		if(oa==0 || ob==0 ) return 0;
+		
+		float tiLe= ob/oa;
+		float radian=(float) Math.atan(tiLe)  ;//radian= arctan(tiLe)
+		float degrees=(float) Math.toDegrees(radian) ; //degress= radian*(180/pi)
+		if(a.giaTriX()*b.giaTriY()>0) return 180-degrees;
+		else return degrees;
+	}
+//======================================================================//		
 	public static void main(String[] args) {
-//		Diem a= new Diem(3,2);
-//		Diem b= new Diem(4,5);
+//		Diem a= new Diem(5,-4);
+//		Diem b= new Diem(5,4);
 //		DoanThang d1 = new DoanThang(a,b);
-//		d1.tinhTienDoanThang(1, 1);
+//		System.out.println(d1.tinhGocTrucHoanh());
 //		d1.hienThi();
-//		System.out.println(d1.tinhDoDai());
 	}
 }
