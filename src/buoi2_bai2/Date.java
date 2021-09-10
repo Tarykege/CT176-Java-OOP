d7637bdacb6cf5ff5347c049b18c364bcebe179a
Tarykege <xoiiwkingg@gmail.com>
2021-09-10 11:10:59 +0700

    Complete_congNgay_method

--- src/buoi2_bai2/Date.java
+++ src/buoi2_bai2/Date.java
@@ -39,18 +39,22 @@ public class Date {
 		if (thang==1 || thang==3 || thang==5 || thang==7 || thang==8 || thang==10 || thang==12){
 			return 31;
 		}
-		else if (thang==2) {//neu dung 1 va 2 thi false
+		else if (thang==2) {
 			if((nam % 4 !=0 || nam % 100 ==0) && nam % 400 !=0) {
 				return 28;
 			}
-			else 
-				return 29;
-		} else 
-			return 30;
+			else return 29;
+			
+		} else return 30;
 	}
 //======================================================================//
 	public boolean hopLe() {
 		if(this.ngay>lastMonth(this.thang,this.nam) || this.ngay < 1) 
+			return false; else return true;
+	}
+//===============================Overload================================//
+	public boolean hopLe(int thang, int nam) {
+		if(ngay>lastMonth(thang,nam) || ngay < 1) 
 			return false; else return true;
 	}
 //======================================================================//
@@ -59,6 +63,30 @@ public class Date {
 		int d=this.ngay,m=this.thang,y=this.nam;
 		
 		if(hopLe()) {
+			if(d==lastMonth(m,y)) {
+				if(m==12) {
+					d=1;
+					m=1;
+					y+=1;
+				} else {
+					d=1;
+					m+=1;
+				  //y=this.nam;
+				}
+			} else {
+				d+=1;
+			  //m=this.thang;
+			  //y=this.nam;
+			}			
+		}
+		return tomorrow = new Date(d, m, y);
+	}
+//===============================Overload================================//
+	public Date ngayHomSau(int ngay, int thang, int nam) {
+		Date tomorrow;
+		int d=ngay,m=thang,y=nam;
+		
+		if(hopLe(m,y)) {
 			if(d==lastMonth(m,y)) {
 				if(m==12) {
 					d=1;
@@ -79,24 +107,29 @@ public class Date {
 	}
 //======================================================================//	
 	public Date congNgay(int n) {
+		Date newDate;
 		int d=this.ngay, m=this.thang, y= this.nam;
-		n= n-(lastMonth(m,y)-d);
-		while(n>0) {
-			m++;
-			if(m>12) {
-				m=1;
-				y++;
+		do {
+			n-=(lastMonth(m,y)-d);
+			if(n>0) {
+				newDate = ngayHomSau(lastMonth(m,y),m,y);
+				n-=1;
+				d=newDate.ngay;
+				m=newDate.thang;
+				y=newDate.nam;
 			}
-			n= n-lastMonth(m,y);
-		}
-		d = (lastMonth(m,y)-n)-lastMonth(m,y);
-		Date newDate = new Date(d,m,y);
+			else {
+				n+=lastMonth(m,y)-d;
+				newDate = new Date(d+n,m,y);
+				break;
+			}
+		}while(n>0);
 		return newDate;
 	}
 //======================================================================//
 	public static void main(String[] args) {
-		Date d = new Date(2, 12, 2001);
-		d=d.ngayHomSau();
+		Date d = new Date(15, 6, 2001);
+		d=d.congNgay(2);
 		System.out.println(d.toString());
 	}	
 }
