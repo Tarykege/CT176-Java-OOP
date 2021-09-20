f719e01a03013a036d5149c838246fe87e227774
Tarykege <xoiiwkingg@gmail.com>
2021-09-20 10:12:23 +0700

    Done

--- src/buoi3_bai1/Gach.java
+++ src/buoi3_bai1/Gach.java
@@ -3,86 +3,117 @@ package buoi3_bai1;
 import java.util.Scanner;
 
 public class Gach {
-	String maSo;
-	String mau;
-	int soLuongVien,chieuDai,chieuRong;
-	long giaBan;
-	Gach(){}
+	private String maSo;
+	private String mau;
+	private int soLuongVien,chieuDai,chieuRong;
+	private long giaBan;
+
+//======================================================================//
+	Gach(){
+		this.maSo=new String();
+		this.mau= new String();
+		this.soLuongVien=0;
+		this.chieuDai=0;
+		this.chieuRong=0;
+		this.giaBan=0;
+	}
+	Gach(String maSo, String mau, int soLuongVien, int chieuDai, int chieuRong, long giaBan){
+		this.maSo=maSo;
+		this.mau=mau;
+		this.soLuongVien=soLuongVien;
+		this.chieuDai=chieuDai;
+		this.chieuRong=chieuRong;
+		this.giaBan=giaBan;
+	}
+	Gach(Gach obj){
+		this.maSo=obj.maSo;
+		this.mau=obj.mau;
+		this.soLuongVien=obj.soLuongVien;
+		this.chieuDai=obj.chieuDai;
+		this.chieuRong=obj.chieuRong;
+		this.giaBan=obj.giaBan;
+	}
+//======================================================================//
+	public String getMaSo() {
+		return maSo;
+	}
+	public String getMau() {
+		return mau;
+	}
+	public int getSoLuongVien() {
+		return soLuongVien;
+	}
+	public int getChieuDai() {
+		return chieuDai;
+	}
+	public int getChieuRong() {
+		return chieuRong;
+	}
+	public long getGiaBan() {
+		return giaBan;
+	}
+//======================================================================//
+	public void setMaSo(String maSo) {
+		this.maSo = maSo;
+	}
+	public void setMau(String mau) {
+		this.mau = mau;
+	}
+	public void setSoLuongVien(int soLuongVien) {
+		this.soLuongVien = soLuongVien;
+	}
+	public void setChieuDai(int chieuDai) {
+		this.chieuDai = chieuDai;
+	}
+	public void setChieuRong(int chieuRong) {
+		this.chieuRong = chieuRong;
+	}
+	public void setGiaBan(long giaBan) {
+		this.giaBan = giaBan;
+	}
+//======================================================================//	
 	void nhapThongTin() {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Nhap ma so: ");
-		String maSo= scanner.nextLine();
-		this.maSo=maSo;
+		this.maSo= scanner.nextLine();
 		System.out.print("Nhap mau: ");
-		String mau= scanner.nextLine();
-		this.mau=mau;
+		this.mau= scanner.nextLine();
 		System.out.print("Nhap so luong vien trong hop: ");
-		int soLuongVien= scanner.nextInt();
-		this.soLuongVien=soLuongVien;
+		this.soLuongVien= scanner.nextInt();
 		System.out.print("Nhap chieu dai 1 vien gach (cm): ");
-		int chieuDai= scanner.nextInt();
-		this.chieuDai=chieuDai;
+		this.chieuDai= scanner.nextInt();
 		System.out.print("Nhap chieu rong 1 vien gach (cm): ");
-		int chieuRong= scanner.nextInt();
-		this.chieuRong=chieuRong;
+		this.chieuRong= scanner.nextInt();
 		System.out.print("gia ban/hop: ");
-		long giaBan= scanner.nextInt();
-		this.giaBan=giaBan;
+		this.giaBan= scanner.nextInt();
 	}
 	
-	String hienThiThongTin() {
-		return this.maSo+" - "+this.mau+" - "+this.soLuongVien+" - "+this.chieuDai+"cm"+" - "+this.chieuRong+"cm"+" - "+this.giaBan;
+	@Override
+	public String toString() {
+		return "Ma so: "+this.maSo+"; "+"Mau: "+this.mau+"; "+"So luong vien: "+this.soLuongVien+"; "+"Dai: "+this.chieuDai+"cm"+"; "+"Rong: "+this.chieuRong+"cm"+"; "+"Gia/hop: "+this.giaBan;
 	}
 	
 	float giaBanLe() {
-		float cost= (this.giaBan*20/100)/this.soLuongVien;
-		     cost+=(this.giaBan/this.soLuongVien);
-		return cost;
+		return (this.giaBan/this.soLuongVien)+(this.giaBan/this.soLuongVien)*((float)20/100);
 	}
-	
-	float dienTichNenToiDa() {
-		//lay dien tich cua 1 vien * so luong 1 hop (cm2)
-		long cm2=this.chieuDai*this.chieuRong*this.soLuongVien;
-		double m2=cm2*Math.pow(10, -3);
-		return (float) m2;
+//======================================================================//
+	int dienTichNenToiDa() {
+		return this.chieuDai*this.chieuRong*this.soLuongVien; //dien tich 1 vien * so luong 1 hop (cm2)
 	}
-	
-	double soLuongLot(double D, double N) {
-		//dien tich D*N / dien tich 1 vien gach (m2)
-		double sYeuCau=D*N;
-		double sGach=(this.chieuDai*this.chieuRong)*Math.pow(10, -3);
-		return sYeuCau/sGach;
+//======================================================================//
+	int soLuongHop(int D, int N) {
+		Gach gach = new Gach(this.maSo,this.mau,this.soLuongVien,this.chieuDai,this.chieuRong,this.getGiaBan());
+		Double sMax=gach.dienTichNenToiDa()*Math.pow(10,-4); //cm2 -> m2
+		return (int)Math.ceil(D*N/sMax); //dien tich D*N / dien tich 1 hop gach (m2)
 	}
-	
-	//overload
-	double soLuongLot(double D, int N) {
-		//dien tich D*N / dien tich 1 vien gach (m2)
-		double sYeuCau=D*N;
-		double sGach=(this.chieuDai*this.chieuRong)*Math.pow(10, -3);
-		return sYeuCau/sGach;
-	}
-	
-	//overload
-	double soLuongLot(int D, int N) {
-		//dien tich D*N / dien tich 1 vien gach (m2)
-		double sYeuCau=D*N;
-		double sGach=(this.chieuDai*this.chieuRong)*Math.pow(10, -3);
-		return sYeuCau/sGach;
-	}
-	
-	//overload
-	double soLuongLot(int D, double N) {
-		//dien tich D*N / dien tich 1 vien gach (m2)
-		double sYeuCau=D*N;
-		double sGach=(this.chieuDai*this.chieuRong)*Math.pow(10, -3);
-		return sYeuCau/sGach;
-	}
+//======================================================================//
 	public static void main(String[] args) {
-//		Gach gach= new Gach();
-//		gach.nhapThongTin();
-//		System.out.println(gach.hienThiThongTin());
-//		System.out.println(gach.giaBanLe());
-//		System.out.println(gach.dienTichNenToiDa());
-//		System.out.println(Math.ceil(gach.soLuongLot(14.7, 2)));
+		Gach gach= new Gach();
+		gach.nhapThongTin();
+		System.out.println(gach);
+		System.out.println(gach.giaBanLe());
+		System.out.println(gach.chieuDai*gach.chieuRong);
+		System.out.println(gach.dienTichNenToiDa()*Math.pow(10,-4));
+		System.out.println(gach.soLuongHop(4,5));
 	}
 }
--- src/buoi3_bai1/SDGach.java
+++ src/buoi3_bai1/SDGach.java
@@ -1,41 +1,38 @@
 package buoi3_bai1;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class SDGach {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("Nhap danh sach loai gach: ");
+		
+		System.out.print("Nhap so loai gach: ");
 		int n= scanner.nextInt();
-		List<Gach> gachs = new ArrayList<Gach>();
+		Gach[] dsGach = new Gach[n];
+		
 		for(int i=0; i<n; i++) {
-			Gach gach= new Gach();
-			System.out.println("Nhap loai "+(i+1)+" ");
-			gach.nhapThongTin();
-			gachs.add(gach);
+			System.out.println("Loai "+(i+1));
+			dsGach[i]= new Gach();
+			dsGach[i].nhapThongTin();
 		}
 		
 		for(int i=0; i<n; i++) {
-			System.out.println("Loai "+i+": "+gachs.get(i).hienThiThongTin());
+			System.out.println("Loai "+i+": {"+dsGach[i].toString()+"}");
 		}
 		
 		float min=Float.MAX_VALUE;
 		int p=0;
 		for(int i=0; i<n; i++) {
-			if(gachs.get(i).giaBanLe()<min) {
-				min=gachs.get(i).giaBanLe();
+			if(dsGach[i].giaBanLe()<min) {
+				min=dsGach[i].giaBanLe();
 				p=i;
 			}
 		}
-		System.out.println("gach chi phi lot thap nhat: "+gachs.get(p).hienThiThongTin());
+		System.out.println("gach chi phi lot thap nhat: {"+dsGach[p].toString()+"}");
 		
 		//gia ban le * so luong lot toi da 
 		for(int i=0; i<n; i++) {
-//			System.out.println(gachs.get(i).giaBanLe());
-//			System.out.println(Math.ceil(gachs.get(i).soLuongLot(20, 5)));
-			System.out.println("chi phi mua gach: "+gachs.get(i).giaBanLe()*Math.ceil(gachs.get(i).soLuongLot(20, 5)));
+			System.out.println("chi phi mua gach: "+dsGach[i].soLuongHop(20, 5)*dsGach[i].getGiaBan());
 		}
 	}
 }
