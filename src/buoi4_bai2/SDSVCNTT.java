76ede056d5fbee9051bab8c7cce0d8dab20c086b
Tarykege <xoiiwkingg@gmail.com>
2021-09-29 10:43:12 +0700

    buoi4_bai2_done

--- src/buoi4_bai2/SDSVCNTT.java
+++ src/buoi4_bai2/SDSVCNTT.java
@@ -1,5 +1,44 @@
 package buoi4_bai2;
 
-public class SDSVCNTT {
+import java.util.Scanner;
 
+public class SDSVCNTT {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Nhap so luong SV CNTT: ");
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		SVCNTT[] dsSVCNTT = new SVCNTT[n];
+		
+		for(int i=0; i<n; i++) dsSVCNTT[i] = new SVCNTT();
+		for(int i=0; i<n; i++) {
+			System.out.println("Nhap thong tin sinh vien thu "+(i+1));
+			dsSVCNTT[i].nhap();
+		}
+		
+		for(int i=0; i<n; i++) {
+			System.out.println("Nhap mon va diem sinh vien thu "+(i+1));
+			dsSVCNTT[i].nhapDiemHocPhan();
+		}
+		
+		System.out.println("In danh sach sinh vien CNTT");
+		for (SVCNTT svcntt : dsSVCNTT) {
+			System.out.println(svcntt);
+		}
+		
+		String timTK = new String();
+		boolean found=false;
+		System.out.print("Nhap ten tai khoan email can tim: ");
+		timTK= scanner.nextLine();
+		for(int i=0; i<n; i++) {
+			if(dsSVCNTT[i].getEmail().compareToIgnoreCase(timTK)==0) {
+				System.out.println("Thong tin sinh vien tim duoc");
+				System.out.println(dsSVCNTT[i]+", diemTB: "+dsSVCNTT[i].diemTB4());
+				found=true;
+				break;
+			}
+		}
+		if(!found) System.out.println("Khong tim duoc sinh vien co tai khoan email nay!");
+	}
 }
--- src/buoi4_bai2/SVCNTT.java
+++ src/buoi4_bai2/SVCNTT.java
@@ -1,5 +1,7 @@
 package buoi4_bai2;
 
+import java.util.Scanner;
+
 import buoi2_bai2.Date;
 import buoi3_bai3.SinhVien;
 
@@ -7,7 +9,8 @@ public class SVCNTT extends SinhVien {
 	String tk;
 	String mk;
 	String email;
-	
+
+//======================================================================//
 	public SVCNTT() {
 		super();
 		this.tk=new String();
@@ -28,4 +31,50 @@ public class SVCNTT extends SinhVien {
 		this.mk=new String(obj.mk);
 		this.email=new String(obj.email);
 	}
+//======================================================================//
+	public void nhap() {
+		Scanner scanner = new Scanner(System.in);
+		super.nhap();
+		System.out.print("Nhap tai khoan: ");
+		this.tk=scanner.nextLine();
+		System.out.print("Nhap mat khau: ");
+		this.mk=scanner.nextLine();
+		System.out.print("Nhap email: ");
+		this.email=scanner.nextLine();
+	}
+//======================================================================//	
+	@Override
+	public String toString() {
+		return super.toString()+", TK: "+this.tk+", MK: "+this.mk+", Email: "+this.email;
+	}
+//======================================================================//
+	public String getTk() {
+		return tk;
+	}
+	public String getMk() {
+		return mk;
+	}
+	public String getEmail() {
+		return email;
+	}
+//======================================================================//
+	public void setTk(String tk) {
+		this.tk = tk;
+	}
+	public void setMk(String mk) {
+		this.mk = mk;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+//======================================================================//
+//	public static void main(String[] args) {
+//		SVCNTT a= new SVCNTT();
+//		a.nhap();
+//		a.nhapDiemHocPhan();
+//		System.out.println(a);
+//		a.setMk("MotNgayNangDepRangNgoi");
+//		System.out.println(a);
+//		System.out.println(a.getEmail());
+//	}
 }
